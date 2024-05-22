f2e68a8383856470045d953675ff04596d1e9380
kyljmeeski <kyljmeeski@gmail.com>
2024-05-22 20:16:26 +0600

    feat: create declare the queue "vacancies-to-notify" and start consuming as defined in NotifyVacancyJob

--- src/main/java/com/kyljmeeski/vacanciesbot/notifier/Main.java
+++ src/main/java/com/kyljmeeski/vacanciesbot/notifier/Main.java
@@ -1,9 +1,11 @@
 package com.kyljmeeski.vacanciesbot.notifier;
 
+import com.kyljmeeski.rabbitmqwrapper.Consumer;
+import com.kyljmeeski.rabbitmqwrapper.PlainConsumer;
 import com.kyljmeeski.rabbitmqwrapper.Queues;
+import com.kyljmeeski.rabbitmqwrapper.RabbitQueue;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Consumer;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -18,9 +20,11 @@ public class Main {
         try {
             Connection connection = factory.newConnection();
             Queues queues = new Queues(connection);
-            queues.declare(
+            RabbitQueue queue = queues.declare(
                     "vacancies-to-notify", false, false, false, null
             );
+            Consumer consumer = new PlainConsumer(connection, queue, new NotifyVacancyJob());
+            consumer.startConsuming();
         } catch (IOException | TimeoutException e) {
             throw new RuntimeException(e);
         }

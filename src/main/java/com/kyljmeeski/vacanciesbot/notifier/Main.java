84b53c71bc1d836514771c171d5980acc5f748cf
kyljmeeski <kyljmeeski@gmail.com>
2024-05-24 21:00:07 +0600

    feat: establish postgres connection

--- src/main/java/com/kyljmeeski/vacanciesbot/notifier/Main.java
+++ src/main/java/com/kyljmeeski/vacanciesbot/notifier/Main.java
@@ -1,13 +1,12 @@
 package com.kyljmeeski.vacanciesbot.notifier;
 
-import com.kyljmeeski.rabbitmqwrapper.Consumer;
-import com.kyljmeeski.rabbitmqwrapper.PlainConsumer;
-import com.kyljmeeski.rabbitmqwrapper.Queues;
-import com.kyljmeeski.rabbitmqwrapper.RabbitQueue;
+import com.kyljmeeski.rabbitmqwrapper.*;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.concurrent.TimeoutException;
 
 public class Main {
@@ -17,15 +16,31 @@ public class Main {
         factory.setHost("localhost");
         factory.setPort(5672);
 
+        String url = "jdbc:postgresql://localhost:5432/vacancies-bot";
+        String user = "postgres";
+        String password = "postgres";
+
         try {
-            Connection connection = factory.newConnection();
-            Queues queues = new Queues(connection);
-            RabbitQueue queue = queues.declare(
-                    "vacancies-to-notify", false, false, false, null
-            );
-            Consumer consumer = new PlainConsumer(connection, queue, new NotifyVacancyJob());
-            consumer.startConsuming();
-        } catch (IOException | TimeoutException e) {
+            java.sql.Connection PostgreSQLConnection = DriverManager.getConnection(url, user, password);
+            Users users = new Users(PostgreSQLConnection);
+            try {
+                Connection rabbitMQConnection = factory.newConnection();
+                Queues queues = new Queues(rabbitMQConnection);
+                RabbitQueue queue = queues.declare(
+                        "vacancies-to-notify", false, false, false, null
+                );
+                Exchanges exchanges = new Exchanges(rabbitMQConnection);
+                RabbitExchange exchange = exchanges.declare("vacancies", "direct");
+                queues.declare(
+                        "telegram-messages", false, false, false, null
+                ).bind(exchange, "telegram-messages");
+                Producer producer = new PlainProducer(rabbitMQConnection, exchange, "telegram-messages");
+                Consumer consumer = new PlainConsumer(rabbitMQConnection, queue, new NotifyVacancyJob(users, producer));
+                consumer.startConsuming();
+            } catch (IOException | TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }

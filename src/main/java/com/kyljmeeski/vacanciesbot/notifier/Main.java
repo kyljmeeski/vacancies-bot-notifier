2bf2f39f779643320d77b132f10697252cca857c
kyljmeeski <kyljmeeski@gmail.com>
2024-07-07 20:11:01 +0600

    update pom.xml; add dockerfile, application.properties

--- src/main/java/com/kyljmeeski/vacanciesbot/notifier/Main.java
+++ src/main/java/com/kyljmeeski/vacanciesbot/notifier/Main.java
@@ -35,13 +35,26 @@ import java.util.concurrent.TimeoutException;
 public class Main {
 
     public static void main(String[] args) {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        factory.setPort(5672);
+        ApplicationProperties applicationProperties = new ApplicationProperties();
 
-        String url = "jdbc:postgresql://localhost:5432/vacancies-bot";
-        String user = "postgres";
-        String password = "postgres";
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(applicationProperties.rabbitMQHost());
+        factory.setPort(applicationProperties.rabbitMQPort());
+
+        String url = String.format(
+                "jdbc:postgresql://%s:%d/%s",
+                applicationProperties.postgresHost(), applicationProperties.postgresPort(),
+                applicationProperties.postgresDB()
+        );
+        String user = applicationProperties.postgresName();
+        String password = applicationProperties.postgresPassword();
+
+        System.out.println(
+                "Notifier is sending messages to be sent to the queue \"telegram-messages\"@" +
+                        applicationProperties.rabbitMQHost() + ":" + applicationProperties.rabbitMQPort() +
+                        " to the users from db@" + applicationProperties.postgresHost() + ":" +
+                        applicationProperties.postgresPort()
+        );
 
         try {
             java.sql.Connection PostgreSQLConnection = DriverManager.getConnection(url, user, password);
